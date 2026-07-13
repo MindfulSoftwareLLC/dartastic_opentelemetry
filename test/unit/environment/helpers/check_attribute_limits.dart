47e3d3450ef8ab77a20c72cd409ee6faebc47ca7
harshitt13 <find.harshitkushwaha@gmail.com>
2026-07-13 15:41:56 +0530

    add helper script
    Signed-off-by: harshitt13 <find.harshitkushwaha@gmail.com>

---
+++ test/unit/environment/helpers/check_attribute_limits.dart
@@ -0,0 +1,15 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+// Helper script: prints JSON of OTelEnv.getAttributeLimits() result.
+// Run via subprocess with OTEL_ATTRIBUTE_* env vars set.
+
+import 'dart:convert';
+
+import 'package:dartastic_opentelemetry/dartastic_opentelemetry.dart';
+
+void main() {
+  OTelLog.logFunction = null;
+  final config = OTelEnv.getAttributeLimits();
+  print(jsonEncode(config));
+}
